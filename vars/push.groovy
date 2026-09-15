c58d30c6d58438efdb10f4943df57a18e3eb4be3
arbajmangaonkar191 <arbajmangaonkar191@gmail.com>
2026-09-15 21:57:26 +0530

    Create push.groovy

---
+++ vars/push.groovy
@@ -0,0 +1,13 @@
+def call(String credentialId, String image) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: $'credentialId',
+            usernameVariable: 'Dockeruser',
+            passwordVariable: 'PasswordHub'
+        )
+    ]) {
+        sh 'docker login -u $Dockeruser -p $PasswordHub'
+        sh "docker image tag ${image} arbajmangaonkar/${image}"
+        sh "docker push arbajmangaonkar/${image}"
+    }
+}
